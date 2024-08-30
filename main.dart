7bea65856aaa464cef9bb2a11b9ad33d9b93b196
Ananyo Dasgupta <dasguptaananyo28@gmail.com>
2024-08-30 14:31:23 +0530

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'login_page.dart'; // Import the login page
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoginPage(), // Set LoginPage as the home screen
+    );
+  }
+}
