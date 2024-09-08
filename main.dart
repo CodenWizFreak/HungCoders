cb4406d31e67370d932586f3ab676cedfa1fb8d2
geothermal-1408 <savvy3526@gmail.com>
2024-09-08 21:17:07 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
-import 'login_page.dart'; // Import the login page
+import 'package:firebase_core/firebase_core.dart';
+import 'screens/login_page.dart'; // Import the login page
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: "AIzaSyB-uozXZzz9pahfODpgYPUDzNjtxmwb0yw",
+          appId: "1:452451009716:android:4fb2709f24f65b26ccc3e1",
+          messagingSenderId: "452451009716",
+          projectId: "crest-f82e8"));
   runApp(MyApp());
 }
 
@@ -13,7 +21,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: LoginPage(), // Set LoginPage as the home screen
+      home: const LoginPage(), // Set LoginPage as the home screen
     );
   }
 }
